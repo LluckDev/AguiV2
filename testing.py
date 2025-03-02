915564568a8ad829b6b758559cebd75d88e6cda4
Lluckmain <Henryshoffacker@gmail.com>
2025-03-01 18:16:56 -0600

    textbox p2

--- testing.py
+++ testing.py
@@ -1,15 +1,9 @@
 from AguiV2 import window
 
 App = window.Window()
+App.rect("b",10,10,50,50,fill="#eeeeee")
+App.textbox("a",10,10,50,50,10,defualtValue="1000000000")
 
-def i():
-    App.set("a","x",App.get("a","x")+1)
-def o():
-    App.set("a","x",App.get("a","x")-1)
-def p():
-    App.set("a","y",App.get("a","y")+1)
-App.rect("a",5,5,25,25)
-App.hitbox("b",0,0,20,20,i,parent="a")
 while App.running:
     App.update()
 
